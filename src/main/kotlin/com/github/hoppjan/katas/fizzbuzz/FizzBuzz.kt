cd524929270243078c10d8930be256ded02773ee
hoppjan <jantorbenhopp@web.de>
2022-04-14 23:00:53 +0200

    Refactored FizzBuzz logic to be more readable

--- src/main/kotlin/com/github/hoppjan/katas/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/com/github/hoppjan/katas/fizzbuzz/FizzBuzz.kt
@@ -1,10 +1,12 @@
 package com.github.hoppjan.katas.fizzbuzz
 
 fun fizzBuzz(dividend: Int) = buildString {
-    if (dividend.rem(3) == 0)
+    if (dividend isDivisibleBy 3)
         append("fizz")
-    if (dividend.rem(5) == 0)
+    if (dividend isDivisibleBy 5)
         append("buzz")
     if (isEmpty())
         append(dividend)
 }
+
+private infix fun Int.isDivisibleBy(divisor: Int) = rem(divisor) == 0
